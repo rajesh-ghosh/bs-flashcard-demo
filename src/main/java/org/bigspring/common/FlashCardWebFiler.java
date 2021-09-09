e53837e474d246a8623d1ab1508690d4e94eb6d9
rajesh-ghosh <ghosh.rajesh@gmail.com>
2021-09-09 17:30:43 +0530

    Reverting allow cross origin

--- src/main/java/org/bigspring/common/FlashCardWebFiler.java
+++ src/main/java/org/bigspring/common/FlashCardWebFiler.java
@@ -5,7 +5,7 @@ import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebFilter("/*")
+//@WebFilter("/*")
 public class FlashCardWebFiler implements Filter {
 
         @Override
